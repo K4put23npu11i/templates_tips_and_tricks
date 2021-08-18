1bc95d3b5c27600da9f974a00c6527bbfe6ea916
K4put23npu11i <K4put23npu11i@gmail.com>
2021-08-18 11:57:59 +0200

    Add folder and file operations

---
+++ folder_and_file_operations/Readme.md
@@ -0,0 +1,14 @@
+# Folder and file Operations
+Collection of functions and code snippet for working with directories and single files
+## What is it?
+- Hashing directories
+- Hashing a single file
+- Reading and Writing files
+
+## How does it work?
+The functions uses some libraries or standard Python functions to perform the actions.
+
+## Sources
+- https://pypi.org/project/checksumdir/
+- https://realpython.com/read-write-files-python/#reading-and-writing-opened-files
+
---
+++ folder_and_file_operations/folder_and_file_operations.py
@@ -0,0 +1,82 @@
+from checksumdir import dirhash
+import os
+import hashlib
+
+
+def build_checksum_of_directory(dir: str, ex_files: list = [], ex_ext: list = [], hash_func: str = 'sha256'):
+    """
+    Builds a checksum of the given directory to check for changes. Uses https://pypi.org/project/checksumdir/
+
+    Parameters
+    ----------
+    dir: str
+        directory to create has of
+    ex_files: list
+        list of files to be excluded of the hash creation
+    ex_ext:
+        list of file extensions to be excluded from the hash creation
+    hash_func: str
+        method of hash algorithm
+
+    Returns
+    -------
+    hash: str
+        hash string of given directory
+    """
+    allowed_hash_functions = ['md5', 'sha1', 'sha256']  # fast, but "insecure" --> slow but more secure
+
+    if os.path.exists(dir) and hash_func in allowed_hash_functions:
+        hash = dirhash(dir, hash_func, excluded_files=ex_files, excluded_extensions=ex_ext)
+        return hash
+    else:
+        return None
+
+
+def build_hash_of_file(filepath: str, hash_func: str = 'sha256'):
+    allowed_hash_functions = ['md5', 'sha1', 'sha256']  # fast, but "insecure" --> slow but more secure
+    buf_size = 65536
+
+    if os.path.exists(filepath) and os.path.isfile(filepath) and hash_func in allowed_hash_functions:
+        hash = eval("hashlib." + hash_func + "()")
+        with open(filepath, 'rb') as f:
+            while True:
+                data = f.read(buf_size)
+                if not data:
+                    break
+                hash.update(data)
+        return hash
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    # testing build_checksum_of_directory
+    directory = './test_directory'
+    dir_hash = build_checksum_of_directory(dir=directory)
+    print(f"Hash of dir: {dir_hash}")
+    dir_hash = build_checksum_of_directory(dir=directory, ex_files=['package.json'])
+    print(f"Hash of dir without package.json: {dir_hash}")
+    dir_hash = build_checksum_of_directory(dir=directory, ex_ext=['txt'])
+    print(f"Hash of dir without txt files: {dir_hash}")
+
+    # testing build_hash_of_file
+    print()
+    filepath = './test_directory/package.json'
+    file_hash1 = build_hash_of_file(filepath=filepath)
+    print(file_hash1)
+    print(file_hash1.hexdigest())
+    filepath = './test_directory/test2.txt'
+    file_hash2 = build_hash_of_file(filepath=filepath)
+    print(file_hash2)
+    print(f"Files are equal: {file_hash1 == file_hash2}")
+
+    # Reading and Writing files
+    # https://realpython.com/read-write-files-python/#reading-and-writing-opened-files
+    filepath_txt = './test_directory/test1.txt'
+    with open(filepath_txt, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    print(lines)
+
+    lines.append("New line :) \n")
+    with open(filepath_txt, 'w') as f:
+        f.writelines(lines)
