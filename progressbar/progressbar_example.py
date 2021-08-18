691aa7a00237c08f776c4799293d557dc413503b
K4put23npu11i <K4put23npu11i@gmail.com>
2021-08-18 12:14:59 +0200

    Formatting files with Numpy style

--- progressbar/progressbar_example.py
+++ progressbar/progressbar_example.py
@@ -31,24 +31,21 @@ if __name__ == "__main__":
     prg_bar_width = 40
     
     define_progress_bar(bar_width=prg_bar_width)
-    
+
     for i in range(0, all_elements_count):
         update_progress_bar(current_count=i, total_count=all_elements_count, bar_width=prg_bar_width)
-        
-        
-# https://towardsdatascience.com/learning-to-use-progress-bars-in-python-2dc436de81e5      
-#import time
-#from progress.bar import IncrementalBar
-#mylist = [1,2,3,4,5,6,7,8]
-#bar = IncrementalBar('Countdown', max = len(mylist))
-#for item in mylist:
+
+    print()
+    print()
+    for i in tqdm(range(0, all_elements_count)):
+        time.sleep(0.1)
+
+# https://towardsdatascience.com/learning-to-use-progress-bars-in-python-2dc436de81e5
+# import time
+# from progress.bar import IncrementalBar
+# mylist = [1,2,3,4,5,6,7,8]
+# bar = IncrementalBar('Countdown', max = len(mylist))
+# for item in mylist:
 #    bar.next()
 #    time.sleep(1)
-#bar.finish()
-
-
-import time
-from tqdm import tqdm
-mylist = [1,2,3,4,5,6,7,8]
-for i in tqdm(mylist):
-    time.sleep(1)
+# bar.finish()
